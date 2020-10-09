380c7f821048c56b4b5623ed8b2f1219e6b0e398
Grant Olson <grant@digitaldreamlabs.com>
2020-10-09 15:56:15 -0400

    More style options for output

--- conf.py
+++ conf.py
@@ -46,7 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'agogo'
-
+html_title = "OSKR Owner's Manual"
+html_logo = "./doc/img/ddl_logo.png"
+    
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
